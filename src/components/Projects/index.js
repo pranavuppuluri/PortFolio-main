d8010d3347b53beff36dfd02405d29bc1d36c223
Pranav <91652265+pranavuppuluri@users.noreply.github.com>
2023-11-11 00:32:26 +0530

    Add files via upload

---
+++ src/components/Projects/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState } from "react";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  CardContainer,
+  ToggleButtonGroup,
+  ToggleButton,
+  Divider
+} from "./ProjectsStyle";
+import ProjectCard from "../Cards/ProjectCards";
+import { projects } from "../../data/constants";
+
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState("all");
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Cerifications & Courses</Title>
+        <Desc>
+          In the dynamic landscape of today's world, continuous learning is the
+          cornerstone of personal and professional growth.
+        </Desc>
+
+        <CardContainer>
+          {toggle === "all" &&
+            projects.map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Projects;
